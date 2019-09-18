ab943ff6ed39622dfad1d16c6f2f68212186e6f6
Heethesh Vhavle <heethesh@gmail.com>
2019-09-18 18:02:28 -0400

    Initial commit

---
+++ src/carla_ros_bridge/radar.py
@@ -0,0 +1,187 @@
+# This script mimics a RADAR sensor. It request all its transformations from the ClientRADAR script.
+# Refer that script for all transformations and bounding box values of all vehicles in world or ego
+# vehicle frame
+
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from radar_utils import *
+
+
+class RADAR:
+    ''' This class instantiates a RADAR object'''
+
+    def __init__(self, theta, r):
+        ''' theta is the angular range of the RADAR.
+        range is the distance up to which RADAR detects objects'''
+        self.theta = theta
+        self.r = r
+
+
+class Vehicle:
+    ''' instantiates a vehicle object. It has x,y of centroid and
+    bounding box with points (x_max, y_max) and (x_min,y_min)'''
+
+    # NOTE: y_max is not actually max y co-ordinate and similarly for y_min
+    # NOTE: Get actual vehicle ID to add as an attribute
+    def __init__(self, obj_id, x, y, z, x_max, y_max, z_max, x_min, y_min, z_min, velocity):
+        self.id = obj_id
+        self.x = x
+        self.y = y
+        self.z = z
+        self.x_max = x_max  # box looks like [[x_max,y], [x_min,y]]
+        self.y_max = y_max
+        self.z_max = z_max
+        self.x_min = x_min
+        self.y_min = y_min
+        self.z_min = z_min
+        self.velocity = velocity
+
+
+def field_of_view_filter(env_vehicles, radar):
+    '''Given a list of vehicles in the environment. This function filters all
+    those vehicles that are not in the field of view(FOV) of the RADAR.
+    It returns a list of vehicles (x,y,yaw) of all vehicles in FOV'''
+    # Iterate over all y values of vehicles
+    filtered_vehicles = []
+    for vehicle in env_vehicles:
+        # Take the left most point of the vehicle and take abs
+        x = vehicle.x_min
+        y = vehicle.y_min
+        # print(vehicle.x, vehicle.y)
+        if x >= 1 / np.tan(radar.theta / 2) * abs(y) and x <= radar.r and x > 0:
+            filtered_vehicles.append(vehicle)
+    return filtered_vehicles
+
+
+def radar_noise(std_dev):
+    # Assume n is in meters. n=1 means that the Radar has std dev of 1 meter.
+    return np.random.normal(0, std_dev)
+
+
+def add_radar_noise(detected_vehicles):
+    n_x = 0.2
+    n_y = 0.5
+    n_v = 0.5
+    # m_x, m_y, m_v = 0.66, 1.50, 2  # change this value for noises after 30m range
+    for vehicle in detected_vehicles:
+        vehicle.y_max += radar_noise(vehicle.x / 50 + n_y)
+        vehicle.y_min += radar_noise(vehicle.x / 50 + n_y)
+        vehicle.y += radar_noise(vehicle.x / 50 + n_y)
+        vehicle.x_max += radar_noise(vehicle.x / 30 + n_x)
+        vehicle.x_min += radar_noise(vehicle.x / 30 + n_x)
+        vehicle.x += radar_noise(vehicle.x / 30 + n_x)
+        vehicle.velocity[0] += radar_noise(vehicle.velocity[0] / 30 + n_v)
+        vehicle.velocity[1] += radar_noise(vehicle.velocity[0] / 30 + n_v)
+
+    return detected_vehicles
+
+
+def radar_detect_vehicles(env_vehicles, radar_detections):
+    '''Returns list of vehicle objects detected by RADAR'''
+
+    fov_vehicles = field_of_view_filter(env_vehicles, radar_detections)
+    if len(fov_vehicles) == 0: return []
+    
+    # Sort vehicles based on x values
+    fov_vehicles = sorted(fov_vehicles, key=lambda x: x.x)
+    
+    # For every detected vehicle my RADAR will not be able to detect the
+    # vehicles other detected vehicles are blocking. Every equation will have a slope
+    slopes = []
+    detected_vehicles = []
+    flag = False
+    
+    # Iterate through FOV vehicles already sorted based on x
+    for vehicle in fov_vehicles:
+        x_max = vehicle.x_max
+        x_min = vehicle.x_min
+        y_max = vehicle.y_max
+        y_min = vehicle.y_min
+        
+        # Find slopes of the detected vehicle
+        m1 = np.tan(x_max / y_max)
+        m2 = np.tan(x_min / y_min)
+        if vehicle == fov_vehicles[0]:
+            slopes.append([m1, m2])
+            detected_vehicles.append(vehicle)
+        else:
+            for m in slopes:
+                if m1 < m[0] or m2 > m[1]:
+                    flag = True
+                else:
+                    flag = False
+        if flag == True:
+            slopes.append([m1, m2])
+            detected_vehicles.append(vehicle)
+    
+    # Adding noise
+    detected_vehicles = add_radar_noise(detected_vehicles)
+    return detected_vehicles
+
+
+def parse_veolcity(ego_vehicle, vehicle):
+    '''Returns velocity with some added noise'''
+    H_W_to_ego = get_car_bbox_transform(ego_vehicle)
+    x_vel = vehicle.get_velocity().x
+    y_vel = -vehicle.get_velocity().y
+    z_vel = vehicle.get_velocity().z
+    # The above values are in the world coordinate frame
+    # Transforming them w.r.t ego vehicle frame
+    vel_vec = np.array([x_vel, y_vel, z_vel, 0])
+    vel_ego = np.matmul(np.linalg.pinv(H_W_to_ego), vel_vec)
+    vel_ego = vel_ego.tolist()
+
+    return vel_ego
+
+
+# This is the final function that needs to run in order to visualize
+# a RADAR output
+def simulate_radar(theta, r, actor_list, ego_vehicle, radar_transform):
+    ''' Simulate and visualize RADAR output'''
+    radar = RADAR(theta, r)
+    env_vehicles = []
+    # Iterate over vehicles in actor list and create vehicle class objects
+    for obj in actor_list:
+        ego_velocity = parse_veolcity(ego_vehicle, obj)
+        x, y, z, x_max, y_max, z_max, x_min, y_min, z_min = get_min_max_bbox(ego_vehicle, obj, radar_transform)
+        # All these values are w.r.t the center of the ego vehicle.
+        # Tranform these values to the RADAR position
+        pose = np.array([[x, y, z, 1], [x_max, y_max, z_max, 1], [x_min, y_min, z_min, 1]])
+        vehicle = Vehicle(obj.id, x, y, z, x_max, y_max, z_max, x_min, y_min, z_min, ego_velocity)
+        env_vehicles.append(vehicle)
+
+    detected_vehicles = radar_detect_vehicles(env_vehicles, radar)    
+    return detected_vehicles
+
+
+def visualize_radar(env_vehicles, detected_vehicles, radar, Truck):
+    '''This function shows a scatter plot of vehicles in environment,
+    the RADAR detected vehicles, and the ego vehicle'''
+    env_plot = [[env_v.x, env_v.y] for env_v in env_vehicles]
+    env_plot = np.asarray(env_plot)
+
+    det_plot = [[det_v.x, det_v.y] for det_v in detected_vehicles]
+    det_plot = np.asarray(det_plot)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(111)
+
+    ax1.scatter(env_plot[:, 0], env_plot[:, 1], s=10, c='b', marker="s", label='All vehicles')
+    if len(detected_vehicles) != 0:
+        ax1.scatter(det_plot[:, 0], det_plot[:, 1], s=10, c='r', marker="o", label='Detected Vehicles')
+    ax1.scatter(Truck.x, Truck.y, s=15, c='g', marker="+", label='Ego Vehicle')
+    
+    # Plot RADAR FOV
+    xlim = np.tan(radar.theta / 2) * radar.r
+    x = np.linspace(-xlim, xlim, 100)
+    y = 1 / np.tan(radar.theta / 2) * abs(x)
+    ax1.plot(x, y, '-r', label='FOV')
+    
+    # Set limits for plot
+    ax1.set_xlim(-100, 100)
+    ax1.set_ylim(-500, 500)
+    plt.legend(loc='upper left')
+    plt.show()
---
+++ src/carla_ros_bridge/radar_publisher.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Author  : Prateek Parmeshwar
+Email   : pparmesh@andrew.cmu.edu
+Version : 1.0.0
+Date    : Apr 08, 2019
+'''
+
+import random
+import threading
+
+import tf
+import carla
+import rospy
+import numpy as np
+from geometry_msgs.msg import Point, Polygon, Vector3
+from radar_msgs.msg import RadarTrack, RadarTrackArray
+
+from carla_ros_bridge.bridge import CarlaRosBridge
+from carla_ros_bridge.bridge_with_rosbag import CarlaRosBridgeWithBag
+from carla_ros_bridge.radar import simulate_radar
+
+RADAR_FRAME = '/ego_vehicle/radar'
+VEHICLE_FRAME = '/ego_vehicle'
+
+
+def list_to_ros_vector3(input_list):
+    output_vector = Vector3()
+    output_vector.x = input_list[0]
+    output_vector.y = input_list[1]
+    output_vector.z = input_list[2]
+    return output_vector
+
+
+def polygon_list_to_ros_points(vehicle):
+    output_polygon = Polygon()
+    
+    mid_point = Point()
+    mid_point.x, mid_point.y, mid_point.z = vehicle.x, vehicle.y, vehicle.z
+    output_polygon.points.append(mid_point)
+    
+    max_point = Point()
+    max_point.x, max_point.y, max_point.z = vehicle.x_max, vehicle.y_max, vehicle.z_max
+    output_polygon.points.append(max_point)
+    
+    min_point = Point()
+    min_point.x, min_point.y, min_point.z = vehicle.x_min, vehicle.y_min, vehicle.z_min
+    output_polygon.points.append(min_point)
+    
+    return output_polygon
+
+
+def publisher(actor_list, ego_vehicle):
+    # Setup node
+    pub = rospy.Publisher('/carla/ego_vehicle/radar/tracks', RadarTrackArray, queue_size=10)
+
+    # Define RADAR parameters
+    theta_range = 2 * np.pi / 3
+    dist_range = 150
+    radar_transform = [2.2, 0, 0.5, 0, 0, 0]
+
+    # Publish at a rate of 13Hz. This is the RADAR frequency
+    r = rospy.Rate(13)
+
+    # Randomly publish some data
+    while not rospy.is_shutdown():
+        msg = RadarTrackArray()
+        br = tf.TransformBroadcaster()
+        br.sendTransform((radar_transform[0], radar_transform[1], radar_transform[2]), \
+                      tf.transformations.quaternion_from_euler(radar_transform[3], radar_transform[4], radar_transform[5]), \
+                      rospy.Time.now(), RADAR_FRAME, VEHICLE_FRAME)
+
+        # Get list of all detected vehicles
+        radar_detections = simulate_radar(theta_range, dist_range,
+            actor_list, ego_vehicle, radar_transform)
+
+        # Iterate over all cars and store their values in RADAR_msgs
+        for detection in radar_detections:
+            radar_track = RadarTrack()
+            # Assigning RADAR ID
+            radar_track.track_id = detection.id
+            # Assingning three detected points
+            radar_track.track_shape = polygon_list_to_ros_points(detection)
+            # Assigning linear velocity
+            radar_track.linear_velocity = list_to_ros_vector3(detection.velocity)
+            # Append to main array
+            msg.tracks.append(radar_track)
+
+        # Header stamp and publish the message
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = RADAR_FRAME
+        pub.publish(msg)
+
+        r.sleep()
+
+
+def main():
+    """
+    main function for carla simulator ROS bridge
+    maintaiing the communication client and the CarlaRosBridge objects
+    """
+    rospy.init_node("radar_client", anonymous=True)
+
+    params = rospy.get_param('carla')
+    host = params['host']
+    port = params['port']
+
+    rospy.loginfo("Trying to connect to {host}:{port}".format(host=host, port=port))
+
+    try:
+        carla_client = carla.Client(host=host, port=port)
+        carla_client.set_timeout(2000)
+
+        carla_world = carla_client.get_world()
+
+        rospy.loginfo("Connected")
+
+        # bridge_cls = CarlaRosBridge
+        # carla_ros_bridge = bridge_cls(
+        #     carla_world=carla_client.get_world(), params=params)
+        # carla_ros_bridge.run()
+        # carla_ros_bridge = None
+        # Get all  vehicle actors in environment
+        actor_list = carla_world.get_actors().filter('vehicle.*')
+        npc_list = []
+
+        # Get ego vehicle object and remove it from actor list
+        ego_vehicle = None
+        for actor in actor_list:
+            attribute_val = actor.attributes['role_name']
+            if attribute_val == 'hero':
+                ego_vehicle = actor
+            else:
+                npc_list.append(actor)
+
+        if ego_vehicle is not None:
+            publisher(npc_list, ego_vehicle)
+
+        rospy.logdebug("Delete world and client")
+        del carla_world
+        del carla_client
+
+    finally:
+        rospy.loginfo("Done")
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        print('ROSInterruptException occurred')
